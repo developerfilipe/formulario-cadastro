f1bbf606edf4c23e50e9b58d85fa766d1864e2af
Filipe Souza Santos <filipe.souza2511@gmail.com>
2024-07-29 19:34:07 -0300

    first commit

---
+++ src/test/java/io/github/com/developerfilipe/formulario_cadastro/FormularioCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.developerfilipe.formulario_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
